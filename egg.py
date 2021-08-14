e06b438e9d95c6b6edfeb74591ece08680c331a9
Mission Marcus <missionmarcus@gmail.com>
2021-08-13 20:11:28 -0500

    made everything more consistent

--- egg.py
+++ egg.py
@@ -2,11 +2,11 @@ import random
 
 class Egg:
 
-    def __init__(self, criticalFloor):
-        self.criticalFloor = criticalFloor
+    def __init__(self, critical_floor):
+        self.critical_floor = critical_floor
 
     def drop(self, floor):
-        if (floor >= self.criticalFloor):
+        if (floor >= self.critical_floor):
             return True
         else:
             return False
@@ -15,13 +15,13 @@ class Egg:
 
 class FloorAlgorithm:
 
-    def __init__(self, floors, criticalFloor):
-        self.egg = Egg(criticalFloor)
+    def __init__(self, floors, critical_floor):
+        self.egg = Egg(critical_floor)
         self.floors = floors
         self.rec_comparisons = 0
         self.iter_comparisons = 0
 
-    def runIterative(self):
+    def run_iterative(self):
 
         for i in range(0, self.floors):
             self.iter_comparisons = self.iter_comparisons + 1
@@ -29,24 +29,24 @@ class FloorAlgorithm:
                 return i
     
 
-    def runRec(self):
-        return self.runRecursive(self.egg, self.floors / 2, self.floors)
+    def run_rec(self):
+        return self.run_recursive(self.egg, self.floors / 2, self.floors)
 
 
-    def runRecursive(self, egg, currentFloor, totalFloors):
+    def run_recursive(self, egg, current_floor, total_floors):
         self.rec_comparisons = self.rec_comparisons + 1
-        if (currentFloor == 0 or (egg.drop(currentFloor) and not egg.drop(currentFloor-1))):
-            return int(currentFloor) # assuming at least one floor will break the egg
-        elif (egg.drop(currentFloor)):
-            return self.runRecursive(egg, currentFloor - int(currentFloor / 2), currentFloor)
+        if (current_floor == 0 or (egg.drop(current_floor) and not egg.drop(current_floor-1))):
+            return int(current_floor) # assuming at least one floor will break the egg
+        elif (egg.drop(current_floor)):
+            return self.run_recursive(egg, current_floor - int(current_floor / 2), current_floor)
         else:
-            return self.runRecursive(egg, currentFloor + int((totalFloors - currentFloor) / 2), totalFloors)
+            return self.run_recursive(egg, current_floor + int((total_floors - current_floor) / 2), total_floors)
 
-    def printHistory(self):
+    def print_history(self):
 
-        actual_critical_floor = self.egg.criticalFloor
-        rec_critical_floor = self.runRec()
-        iter_critical_floor = self.runIterative()
+        actual_critical_floor = self.egg.critical_floor
+        rec_critical_floor = self.run_rec()
+        iter_critical_floor = self.run_iterative()
 
         print("Results of Egg Comparison:\n")
         print(f"Actual Critical Floor: {actual_critical_floor}")
@@ -62,4 +62,4 @@ class FloorAlgorithm:
 
 floor_algo = FloorAlgorithm(100, random.randrange(100))
 
-floor_algo.printHistory()
+floor_algo.print_history()
